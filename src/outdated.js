9616eb21e61f7dce9253895926eb0f94961bc697
Igor_Fedyukin <igor_fedyukin@epam.com>
2019-01-12 15:53:58 +0300

    Add remove functional

--- src/outdated.js
+++ src/outdated.js
@@ -1,16 +1,19 @@
+const {deleteTweet} = require('./twitter');
 const {connectMongoDB, clearDB, getOutdatedTweets} = require('./mongoose');
 
-const uniq = arr => Array.from(new Set(arr));
-
 const removeTweets = async (tweets) => {
-  Promise.resolve();
+  // try {
+  //   await tweets.forEach(deleteTweet);
+  // } catch (err) {
+  //   console.error(err);
+  // }
+  return Promise.resolve();
 };
 
 const outdated = async () => {
   await connectMongoDB();
   const [outdatedTweets, condition] = await getOutdatedTweets();
-  const uniqIds = uniq(outdatedTweets.map(({id_str: id}) => id));
-  await removeTweets(uniqIds);
+  await removeTweets(outdatedTweets);
   await clearDB(condition);
   console.log('Outdated tweets were removed');
   process.exit(0);
